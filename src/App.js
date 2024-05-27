f8c5574de78615890e3ecbb39aa3979ed2af6332
unknown <dsv19621227@gmail.com>
2024-05-27 22:44:24 +0300

    start form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        App
+      </div>
+    </div>
+  );
+}
+
+export default App;
