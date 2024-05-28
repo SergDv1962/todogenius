82b553b079d35a7198838e06f40148eefa53efb8
unknown <dsv19621227@gmail.com>
2024-05-28 13:47:55 +0300

    add suspense with fallback &+ loader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import AppTodo from './pages/app-todo/AppTodo';
-import Layout from './layout/Layout';
-import { ROUTES } from './utils/routes';
-import NotFoundPages from './pages/notfoundpages/NotFoundPages';
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { ROUTES } from "./utils/routes";
+
+import "./App.css";
+
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AppTodo from "./pages/app-todo/AppTodo";
+import Layout from "./layout/Layout";
+import NotFoundPages from "./pages/notfoundpages/NotFoundPages";
+
+import Loader from "./loaders/Loader";
 
 function App() {
   return (
     <div className="App">
       <div className="App-container">
-        <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path={ROUTES.ABOUT} element={<About/>}/>
-            <Route path={ROUTES.TODOAPP} element={<AppTodo/>}/>
-            <Route path='*' element={<NotFoundPages/>}/>
-          </Route>
-        </Routes>
+        <Suspense fallback={<Loader/>}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path={ROUTES.ABOUT} element={<About />} />
+              <Route path={ROUTES.TODOAPP} element={<AppTodo />} />
+              <Route path="*" element={<NotFoundPages />} />
+            </Route>
+          </Routes>
+        </Suspense>
         App
       </div>
     </div>
